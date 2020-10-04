517c497e27cca8c845b647833f064a9703be3247
Pablo Armentano <pablo@simpat.io>
2020-10-04 14:56:52 -0300

    refactor(lib): Removed watch file, unified tasks on builder

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,14 @@ version: 		0.0.1
 last_changes:	.
 —————————————————————————————————————————————————————————
 */
-const {task,series} = require('gulp')
-const config = require('./lib/config')
 const server = require('./lib/server')
-const styles = require('./lib/styles')
-const scripts = require('./lib/scripts')
-const media = require('./lib/media')
-const views = require('./lib/views')
 const parser = require('./lib/parser')
-const watch = require('./lib/watch')
+const builder = require('./lib/builder')
 
-/* ————————————————— INTIIALIZERS —————————————————— */
-
-task('server:start', server.start);
-task('server:reload', server.reload);
-
-task('build:views', views);
-task('build:styles', styles);
-task('build:scripts', scripts);
-task('build:media', media);
-
-task('clean', parser.clean);
-
-task('build', series(
-	'build:views',
-	'build:styles',
-	'build:scripts',
-	'build:media'
-))
-
-task('watch', watch)
+module.exports = {
+	start: server.start,
+	reload: server.reload,
+	clean: parser.clean,
+	build: builder.all,
+	watch: builder.watch
+}
